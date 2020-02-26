a4d27d6cae204db7cd46bf26cfb2899664151242
jjiojio jiojojoijio <helloantonhowareyou@outlook.com>
2020-02-26 12:55:08 +0100

    yes

--- HangMan/MainWindow.xaml.cs
+++ HangMan/MainWindow.xaml.cs
@@ -20,9 +20,48 @@ namespace HangMan
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<String> hiddenWord = new List<string>();
+        List<String> guessedWord = new List<string>();
+        string guessedChar;
+        string hiddenChar;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                guessedChar = GuessWordXAML.Text;
+                if(guessedChar.All(char.IsDigit))
+                {
+                    return;
+                }
+                else
+                {
+                    int i = 0;
+                    foreach(string ch in hiddenWord)
+                    {
+                        if(ch == guessedChar)
+                        {
+                            guessedWord[i] = guessedChar;
+                        }
+
+                        i++;
+                    }
+                }
+
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            hiddenWord.Clear();
+            foreach (char ch in EnterWordXAML.Text)
+            {
+                hiddenWord.Add(ch.ToString());
+            }
         }
     }
 }
